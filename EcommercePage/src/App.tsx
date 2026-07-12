827ab62e79bfc1af0d965a747a68d4898dfdd88d
Stefano Buglione <stefanobc123456@gmail.com>
2026-07-12 17:01:06 -0500

    feat: added home page

--- EcommercePage/src/App.tsx
+++ EcommercePage/src/App.tsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Auth from "./Pages/Auth";
+import Checkout from "./Pages/Checkout";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-  
-    </>
-  )
+    <div className="app">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/auth" element={<Auth />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="*" element></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ EcommercePage/src/components/ProductCard.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import type { Product } from "../data/products";
+
+export default function ProductCard({ product }: { product: Product }) {
+
+    //change Link to redirect to a details page
+  return (
+    <div className="product-card">
+      <img src={product.image} className="product-card-image" />
+      <div className="product-card-content">
+        <h3 className="product-card-name">{product.name}</h3>
+        <h3 className="product-card-price">${product.price}</h3>
+        <div className="product-card-actions">
+          <Link to="/" className="btn btn-secondary">View details</Link> 
+          <button className="btn btn-primary">Head to cart</button>
+        </div>
+      </div>
+    </div>
+  );
+}
