531af6a74665e53f2a63fcf4eba900581b649e6d
Stefano Buglione <stefanobc123456@gmail.com>
2026-07-12 19:28:17 -0500

    Feat: Added authentication functionality

--- EcommercePage/src/App.tsx
+++ EcommercePage/src/App.tsx
@@ -5,19 +5,22 @@ import Home from "./Pages/Home";
 import Auth from "./Pages/Auth";
 import Checkout from "./Pages/Checkout";
 import Navbar from "./components/Navbar";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/auth" element={<Auth />}></Route>
-        <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="*" element></Route>
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/auth" element={<Auth />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route path="*" element></Route>
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
